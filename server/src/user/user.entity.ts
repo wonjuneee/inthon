2e2d4a57079510e6673d0a84665f1e47873fe63b
csoob <csoob_407@naver.com>
2024-11-09 19:41:39 +0900

    bace/mod: entity 참조관계 수정

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -17,12 +17,12 @@ export class User extends CommonEntity {
   @Column()
   contains: number[];
 
-  @OneToOne(()=> Egg)
+  @OneToOne(()=> Egg, egg => egg.id)
   @JoinColumn()
   currentEgg: Egg;
 
   @OneToMany(()=> Egg, egg => egg.id)
   @JoinColumn()
-  eggs: Egg[];
+  eggs: Egg;
 
 }
