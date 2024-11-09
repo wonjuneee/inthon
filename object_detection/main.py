2709d4be517111e99947eaf3856ea5c17fc4447e
deun115 <diddmstj15@naver.com>
2024-11-09 22:49:27 +0900

    fix: object-detection model 변경 및 api 수정

--- object_detection/main.py
+++ object_detection/main.py
@@ -1,4 +1,6 @@
 import os
+import base64
+import pprint
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
@@ -8,24 +10,34 @@ app = FastAPI()
 
 load_dotenv()
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/detr-resnet-50"
+API_URL = "https://api-inference.huggingface.co/models/patrickjohncyh/fashion-clip"
 headers = {"Authorization": f"Bearer {os.getenv('HUGGING_FACE')}"}
 
 class DetectionRequest(BaseModel):
-    file: str
+    imagePath: str
+    labels: str
 
 class DetectionResponse(BaseModel):
-    result: str
+    score: float
 
-def detect_image(url: str):
-    # S3 URL에서 이미지 데이터를 가져옵니다.
-    image_data = requests.get(url).content
-    # Hugging Face API에 이미지 데이터를 전송합니다.
-    response = requests.post(API_URL, headers=headers, data=image_data)
+def detect_image(data):
+    # 이미지 파일을 읽고 base64 인코딩합니다.
+    response = requests.get(data["imagePath"])
+    response.raise_for_status()
+    img_data = response.content
+    
+    payload = {
+        "parameters": {"candidate_labels": [data["labels"]]},
+        "inputs": base64.b64encode(img_data).decode("utf-8")
+    }
+    response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
-
 
 @app.post("/detect", response_model=DetectionResponse)
 async def detect(request: DetectionRequest):
-    output = detect_image(request.file)
-    return {"result": output[0]['label']}
+    output = detect_image({
+        "imagePath": request.imagePath,
+        "labels": request.labels
+    })
+    
+    return {"score": float(output[0]['score'])}
