9332fe2efbc965f2a77222cdcedc64ecee0e8cbf
wonjuneee <tony000709@gmail.com>
2024-11-10 06:58:10 +0900

    hotfix: db 수정

--- server/src/egg/egg.service.ts
+++ server/src/egg/egg.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Egg } from './egg.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class EggService {
   constructor(
     @InjectRepository(Egg)
-    private eggRepository: Repository<Egg>
+    private eggRepository: Repository<Egg>,
+    @Inject(forwardRef(() => UserService))
+    private readonly userService: UserService
   ) {}
 
   async createEgg(): Promise<Egg> {
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -6,8 +6,9 @@ import { UserController } from './user.controller';
 import { EggModule } from 'src/egg/egg.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), EggModule],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => EggModule)],
   providers: [UserService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
