6200d2652125f039522afe8a99bd65cb21857d44
wonhj12 <wonhj12@naver.com>
2024-11-10 01:00:02 +0900

    front/feat: 나비 추억 페이지 api 추가, 더미 데이터 추가가

--- client/src/pages/ButterflyPage.tsx
+++ client/src/pages/ButterflyPage.tsx
@@ -1,7 +1,38 @@
 import { Col, Layout, Row } from 'antd';
 import Spacer from '../components/common/Spacer';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Art } from '../models/art';
 
 const ButterflyPage = () => {
+  // const location = useLocation();
+  // const eggId = location.state.eggId;
+
+  // const [arts, setArts] = useState<Art[]>([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.SERVER_URL}/art/get-total-art?id=${eggId}`)
+  //     .then(res => setArts(res.data))
+  //     .catch(err => console.log(err));
+  // }, []);
+
+  const arts: Art[] = [
+    { id: 0, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+    { id: 1, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+    { id: 2, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+    { id: 3, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+    { id: 4, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+    { id: 5, questionIdx: null, imagePath: '', description: null, createdAt: null, updatedAt: null },
+  ];
+
+  const navigate = useNavigate();
+
+  const handleArtClick = (id: number) => {
+    navigate('/art', { state: { id: id } });
+  };
+
   return (
     <Layout style={{ width: 402, height: 874, background: 'var(--secondary)', position: 'relative', overflow: 'hidden' }}>
       <img src={'/assets/flower.png'} className="bg-img" />
@@ -13,9 +44,10 @@ const ButterflyPage = () => {
         <Spacer height={40} />
 
         <Row gutter={[24, 24]} justify="center">
-          {Array.from({ length: 6 }).map((_, index) => (
+          {Array.from(arts).map((art, index) => (
             <Col key={index} flex="none">
               <button
+                onClick={() => handleArtClick(art.id)}
                 style={{
                   height: '102px',
                   width: '102px',
@@ -27,7 +59,9 @@ const ButterflyPage = () => {
                   cursor: 'pointer',
                   padding: 0,
                 }}
-              ></button>
+              >
+                <img src={art.imagePath!} style={{ width: '102px', height: '102px' }} />
+              </button>
             </Col>
           ))}
         </Row>
