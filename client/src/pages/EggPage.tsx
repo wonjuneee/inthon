2e869d54f746126352ff05c83317b97d2a322ed4
rla-suedeng <sue.gus77@gmail.com>
2024-11-10 00:38:34 +0900

    feat : /eggs/get-eggs API 구현

--- client/src/pages/EggPage.tsx
+++ client/src/pages/EggPage.tsx
@@ -1,9 +1,31 @@
-import { Layout, Typography, Row, Col, Grid, Card } from 'antd';
+import { Layout, Typography, Row, Col, Card } from 'antd';
+import axios from 'axios';
+import { Egg } from '../models/egg';
+import React, { useEffect, useState } from 'react';
 
 const EggPage: React.FC = () => {
+  const [eggList, setEggList] = useState<Egg[]>([]);
+
+  const fetchEggList = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/egg/get-eggs`);
+      if (response.status === 200) {
+        const filteredEggs = response.data.eggs.filter((egg: Egg) => egg.step === 0 && egg.currArt === null);
+        setEggList(filteredEggs);
+      }
+    } catch (error) {
+      console.error('알 데이터를 가져오는 중 오류가 발생했습니다.', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEggList();
+  }, []);
+
   const handleCardClick = (index: number) => {
     alert(`알 ${index + 1}을 선택하셨습니다.`);
   };
+
   return (
     <Layout className="layout">
       <div className="text-center" style={{ paddingBottom: '24px' }}>
@@ -12,8 +34,8 @@ const EggPage: React.FC = () => {
         </Typography.Title>
       </div>
       <Row gutter={[34, 34]} justify="center">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <Col key={index} flex="none">
+        {eggList.map((egg, index) => (
+          <Col key={egg.id} flex="none">
             <button
               onClick={() => handleCardClick(index)}
               style={{
@@ -28,7 +50,20 @@ const EggPage: React.FC = () => {
                 padding: 0,
               }}
             >
-              <Card style={{ height: '100%', width: '100%', borderRadius: '10px', background: 'transparent', boxShadow: 'none', flexShrink: 0 }} />
+              <Card style={{ height: '100%', width: '100%', borderRadius: '10px', overflow: 'hidden', background: 'transparent', boxShadow: 'none', flexShrink: 0 }}>
+                <img
+                  src="/assets/egg.png"
+                  alt="Egg"
+                  style={{
+                    transform: 'scale(1.8) translateY(-33%)',
+                    position: 'relative',
+                    width: '400px', // 이미지 확대
+                    height: 'auto',
+                    objectFit: 'cover',
+                    objectPosition: 'center',
+                  }}
+                />
+              </Card>
             </button>
           </Col>
         ))}
