ea310629ee77424aad5ecca33c855130cdadfbb3
wonjuneee <tony000709@gmail.com>
2024-11-10 10:31:01 +0900

    feat: 강제임

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { EggUserService } from './user.service';
 import { EggController } from './user.controller';
 import { EggModule } from 'src/egg/egg.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Egg } from 'src/egg/egg.entity';
 
 @Module({
-  imports: [EggModule],
+  imports: [TypeOrmModule.forFeature([Egg]), EggModule],
   providers: [EggUserService],
   controllers: [EggController],
   exports: [EggUserService],
 })
-export class UserModule {}
+export class EggUserModule {}
